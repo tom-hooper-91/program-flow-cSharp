2c857e8f5fd3c1e81254213b4754926f7e517ec8
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-16 10:58:53 +1200

    add selection statements

--- 02/demos/start/WiredBrainCoffeeSurveys.Reports/Program.cs
+++ 02/demos/start/WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -1,29 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WiredBrainCoffeeSurveys.Reports
-{
+{ 
     class Program
     {
         static void Main(string[] args)
         {
+            var tasks = new List<string>();
+
             // Calculated Values
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
             double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
             double overallScore = (Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore + Q1Results.ServiceScore) / 4;
 
-            Console.WriteLine($"Response rate = {responseRate}");
-            Console.WriteLine($"Unanswered surveys = {unansweredCount}");
-            Console.WriteLine($"Overall score = {overallScore}");
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                tasks.Add("Investigate coffee recipes and ingredients"); 
+             }
 
-            // Logical Comparisons
-            bool higherCoffeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
-            bool customersRecommend = Q1Results.WouldRecommend >= 7;
-            bool noGranolaYesCappucinio = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino"; 
+            if (overallScore > 8)
+            {
+                tasks.Add("Work with leadership to reward staff");
+            }
+            else
+            {
+                tasks.Add("Work with staff on ways to improve");
+            }
 
-
-            Console.WriteLine($"Coffee score is higher than food score = {higherCoffeScore}");
-            Console.WriteLine($"Customers would recommend us = {customersRecommend}");
-            Console.WriteLine($"Hate Granola, Love Cap = {noGranolaYesCappucinio}");
+            if (responseRate < 0.33)
+            {
+                tasks.Add("Work to improve response rate");
+            }
+            else if (responseRate > 0.33 && responseRate < 0.66)
+            {
+                tasks.Add("Send out free coffee coupon");
+            }
+            else
+            {
+                tasks.Add("Send out discount coffee coupon"); 
+            }
         }
     }
 }
