43d723dd696abee3e2194fce2f9757bfde30f65b
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-16 16:21:05 +1200

    refactor statemenets in GenerateTaskReports method

--- 05/demos/start/WiredBrainCoffeeSurveys.Reports/Program.cs
+++ 05/demos/start/WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -116,37 +116,50 @@ namespace WiredBrainCoffeeSurveys.Reports
                 tasks.Add("Investigate coffee recipes and ingredients.");
             }
 
-            if (overallScore > 8.0)
-            {
-                tasks.Add("Work with leadership to reward staff");
-            }
-            else
-            {
-                tasks.Add("Work with employees for improvement ideas.");
-            }
+            // Shorthand ternery for code below
 
-            if (responseRate < .33)
-            {
-                tasks.Add("Research options to improve response rate.");
-            }
-            else if (responseRate > .33 && responseRate < .66)
-            {
-                tasks.Add("Reward participants with free coffee coupon.");
-            }
-            else
-            {
-                tasks.Add("Rewards participants with discount coffee coupon.");
-            }
+            tasks.Add(overallScore > 8.0 ? "Work with leadership to reward staff" : "Work with employees for improvement ideas");
+
+            //if (overallScore > 8.0)
+            //{
+            //    tasks.Add("Work with leadership to reward staff");
+            //}
+            //else
+            //{
+            //    tasks.Add("Work with employees for improvement ideas.");
+            //}
+
+
+            // Shorthand switch expression for code below
+
+            tasks.Add(responseRate switch {
+                var rate when rate < .33 => "Research options to improve response rate.",
+                var rate when rate > .33 && rate < .66 => "Reward participants with free coffee coupon.",
+                var rate when rate > .66 => "Rewards participants with discount coffee coupon."
+            });
+
+            //if (responseRate < .33)
+            //{
+            //    tasks.Add("Research options to improve response rate.");
+            //}
+            //else if (responseRate > .33 && responseRate < .66)
+            //{
+            //    tasks.Add("Reward participants with free coffee coupon.");
+            //}
+            //else
+            //{
+            //    tasks.Add("Rewards participants with discount coffee coupon.");
+            //}
 
             //Shorthand switch expression of switch statement below
 
-            var newTask = results.AreaToImprove switch
+            tasks.Add(results.AreaToImprove switch
             {
                 "RewardsProgram" => "Revisit the rewards deals.",
                 "Cleanliness" => "Contact the cleaning vendor.",
                 "MobileApp" => "Contact the consulting firm about the app.",
                 _ => "Investigate individual commens for ideas."
-            };
+            });
 
             //switch (results.AreaToImprove)
             //{
